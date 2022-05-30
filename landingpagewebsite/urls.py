48b1889dfbdabfdbe72f58f6b0341d8c373c8d89
YOUR_NAME <YOUR_EMAIL>
2022-05-30 22:40:25 +0500

    first_commit

--- landingpagewebsite/urls.py
+++ landingpagewebsite/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from crm import views
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', views.first_page),
+    path('test/', views.thanks_page, name='thanks_page1'),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
