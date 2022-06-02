552da585c53ec77a4df632967618d29b93c68811
YOUR_NAME <YOUR_EMAIL>
2022-06-02 21:53:03 +0500

    first_commit

--- crm/admin.py
+++ crm/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import order
+from .models import order, StatusCrm, CommentCrm
 # Register your models here.
 
 admin.site.register(order)
+admin.site.register(StatusCrm)
+admin.site.register(CommentCrm)
+
